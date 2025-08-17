bd8dc3d018189cfe747289fadc5f82e657f6bf48
= <viktor.brandmaier@icloud.com>
2025-08-17 13:25:08 +0200

    added crossing detection + testing visualisation

--- src/Game.cpp
+++ src/Game.cpp
@@ -9,6 +9,7 @@
 #include <SFML/Window/Event.hpp>
 
 
+
 Game::Game(unsigned _windowSizeX, unsigned _windowSizeY, const std::string& _title):
 m_borderX(_windowSizeX),
 m_borderY(_windowSizeY),
@@ -31,6 +32,8 @@ m_grid(
     m_window.setFramerateLimit(144);
 }
 
+
+
 /*
   factory method to automatically create instances of the correct child-class and add them to
   the base class array in the Game class via polymorphism
@@ -40,14 +43,72 @@ void Game::addEnemy(const std::string& _filePath, sf::Vector2u _spawnPosition) {
     m_pEntities.push_back(pEntity);
 }
 
-void Game::addBorder(sf::Vector2f _spawnPosition, float _tileSize) {
+void Game::addBorder(sf::Vector2f _spawnPosition, float _tileSize, sf::Color _color) {
     sf::RectangleShape* rect = new sf::RectangleShape({_tileSize, _tileSize});
     rect->setPosition(_spawnPosition);
     rect->setFillColor(sf::Color::Black);
-    rect->setOutlineColor(sf::Color::Blue);
+    rect->setOutlineColor(_color);
     rect->setOutlineThickness(5.f);
     m_pBorders.push_back(rect);
 }
+
+
+bool Game::validCrossing(int _pX, int _pY) {
+    //check if current tile itself is empty
+    if (m_grid[_pY][_pX] == '#') {
+        return false;
+    }
+    //set directions for surrounding tiles
+    std::vector<std::pair<int, int>> directions = {
+        {0, -1}, //top
+        {0, 1},  //bottom
+        {-1, 0}, //left
+        {1,0}   //right
+    };
+    //find valid index of neighbouring tiles
+    std::vector<std::pair<int, int>> neighbours = {};
+    for (size_t k = 0; k < directions.size(); ++k) {
+        int dx = directions[k].first;
+        int dy = directions[k].second;
+
+        int nx = _pX + dx;
+        int ny = _pY + dy;
+
+        if (ny >= 0 && ny < m_grid.size() && nx >= 0 && nx < m_grid[0].size()) {
+            if (m_grid[ny][nx] != '#') {
+                neighbours.push_back({nx, ny});
+            }
+        }
+    }
+
+    //if there are less than two neighbours -> no crossing possible
+    if (neighbours.size() < 2) {
+        return false;
+    }
+
+    //if there are excactly two neighbours -> check if they are orthogonal
+    if (neighbours.size() == 2) {
+        //get both neighbours
+        std::pair<int, int> pair1 = neighbours[0];
+        std::pair<int, int> pair2 = neighbours[1];
+
+        //extract the directions
+        int dx1 = pair1.first - _pX;
+        int dy1 = pair1.second - _pY;
+        int dx2 = pair2.first - _pX;
+        int dy2 = pair2.second - _pY;
+
+        //check if they are orthogonal
+        if (dx1 == -dx2 && dy1 == -dy2) {
+            return false;
+        }
+        return true;
+    }
+    //if there are more than two neighbours -> crossing
+    return true;
+}
+
+
 
 void Game::initialize() {
     float tileSize = 80;
@@ -56,6 +117,16 @@ void Game::initialize() {
     for (int i = 0; i < m_grid.size(); ++i) {
         for (int j = 0; j < m_grid[0].size(); ++j) {
             char curr  = m_grid[i][j];
+            if (validCrossing(j, i)) {
+                m_crossings.insert({j,i});
+            }
+            //TESTING ONLY (highlight crossing location in grid via hashing)
+            if (m_crossings.find({j,i}) != m_crossings.end()) {
+                float pX = j* tileSize + 0.5*tileSize;
+                float pY = i* tileSize + 0.5*tileSize;
+                addBorder({pX, pY}, tileSize, sf::Color::Red);
+            }
+            //initialize game map
             switch (curr) {
                 case 'P': {
                     unsigned px = j*tileSize + 0.5*tileSize;
@@ -72,7 +143,7 @@ void Game::initialize() {
                 case '#': {
                     float bx = j*tileSize + 0.5*tileSize;
                     float by = i*tileSize + 0.5*tileSize;
-                    addBorder({bx, by}, tileSize);
+                    addBorder({bx, by}, tileSize, sf::Color::Blue);
                     break;
                 }
                 case '.': {
@@ -86,6 +157,7 @@ void Game::initialize() {
     Entity* pPlayer = Player::get();
     m_pEntities.push_back(pPlayer);
 }
+
 void Game::render() {
 
     m_window.clear();
@@ -106,9 +178,6 @@ void Game::render() {
     }
     m_window.display();
 }
-
-
-
 
 void Game::run() {
     //game-loop
--- src/Game.h
+++ src/Game.h
@@ -6,7 +6,7 @@
 #include <vector>
 #include <SFML/Graphics.hpp>
 #include <string>
-
+#include <unordered_set>
 
 class Entity;
 
@@ -21,7 +21,7 @@ public:
     void render();
     void handleInput();
     void addEnemy(const std::string& _filePath, sf::Vector2u _spawnPosition);
-    void addBorder(sf::Vector2f _spawnPosition, float _tileSize);
+    void addBorder(sf::Vector2f _spawnPosition, float _tileSize, sf::Color _color);
 
 
     std::vector<std::string>* getGrid();
@@ -29,10 +29,24 @@ public:
 
 
 private:
+    //private helper method to find crossings in the grid
+    bool validCrossing(int _pX, int _pY);
+
+    struct m_pairHash {
+        //creates hash for std::pair
+        std::size_t operator() (std::pair<int, int> _pair) {
+            return _pair.first * 42 + _pair.second;
+        }
+    };
+
     std::vector<std::string> m_grid;
     std::vector<sf::RectangleShape*> m_pBorders;
     std::vector<Entity*> m_pEntities;
     sf::RenderWindow m_window;
+
+    //hash set which contains the grid coordinates of all valid crossings
+    std::unordered_set<std::pair<int,int>, m_pairHash> m_crossings;
+
     unsigned m_borderX;
     unsigned m_borderY;
 
