c386cabbea5d6e81e7420b4345f5f3d9a7edde9e
= <viktor.brandmaier@icloud.com>
2025-09-11 21:30:12 +0200

    added dataset creation

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -4,13 +4,21 @@ import json
 import math
 
 dirpath = os.path.normpath(os.path.realpath(__file__))
-processedSessoinsPath = os.path.normpath(os.path.join(dirpath, "../../../data/processed/sessions"))
+processedDataPath = os.path.normpath(os.path.join(dirpath, "../../../data/processed"))
 processedSessionsManifestPath = os.path.normpath(os.path.join(dirpath, "../../../data/processed/manifest.jsonl"))
+
+trainDatasetPath = os.path.normpath(os.path.join(dirpath, "../../../data/datasets/train.parquet"))
+testDatasetPath = os.path.normpath(os.path.join(dirpath, "../../../data/datasets/test.parquet"))
 
 sID_hash = {}
 
 #inverse golden ratio for even distribution
 alpha = (math.sqrt(5) - 1) / 2
+
+threshold = 0.8
+
+trainingSplit = []
+testSplit = []
 
 with open(processedSessionsManifestPath, "r") as f: 
     for line in f:
@@ -19,19 +27,23 @@ with open(processedSessionsManifestPath, "r") as f:
         
         #distributed deterministic hash of session-IDs for better splitting 
         hashedID = (session_id * alpha) % 1
-        sID_hash[hashedID] = session_id
+        sessionPathRelative = jsonObj["file_path"]
+        sessionPathAbsolute = os.path.normpath(os.path.join(processedDataPath, sessionPathRelative))
+
+        #split the processed data into testing and training split
+        session_df = pd.read_parquet(sessionPathAbsolute) 
+        if hashedID < threshold:
+            trainingSplit.append(session_df)
+        else:
+            testSplit.append(session_df)
+
+#concat all the session dataframes into a singular dataframe
+test_df = pd.concat(testSplit)
+train_df = pd.concat(trainingSplit)
+
+#write the new datasets into a singular parquet file
+test_df.to_parquet(testDatasetPath)
+train_df.to_parquet(trainDatasetPath)
 
 
-
-#create a training and testing split based on the session-ID hashes
-threshold = 0.8
-
-trainingSplit = []
-testSplit = []
-
-for key in sID_hash.keys():
-    if key > threshold:
-        testSplit.append(sID_hash[key])
-    else:
-        trainingSplit.append(sID_hash[key])
 
