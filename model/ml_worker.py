a2b3bd7bc409924162aa10750eb9273d206515a2
viktor brandmaier <viktor.brandmaier@icloud.com>
2025-09-24 09:56:34 +0200

    cleaned up code

--- model/ml_worker.py
+++ model/ml_worker.py
@@ -4,35 +4,31 @@ import pandas as pd
 import math
 
     
-    
-for line in sys.stdin: 
+def cleanData(_df):
 
-    snapshot = json.loads(line)
-    
-    #write snapshot into dataframe
-    df = pd.json_normalize(snapshot)
+    _df.dropna()
 
     #split player screen position coordinates into seperate columns
 
-    playerScreenX = df["player_position_screen"].str[0].iloc[0]
-    df["player_position_screenX"] = playerScreenX
-    playerScreenY = df["player_position_screen"].str[1].iloc[0]
-    df["player_position_screenY"] = playerScreenY
+    playerScreenX = _df["player_position_screen"].str[0].iloc[0]
+    _df["player_position_screenX"] = playerScreenX
+    playerScreenY = _df["player_position_screen"].str[1].iloc[0]
+    _df["player_position_screenY"] = playerScreenY
 
 
     #convert enemy_momenta to dataframe
-    enemy_momenta = pd.DataFrame(df["enemy_momenta"].tolist())
+    enemy_momenta = pd.DataFrame(_df["enemy_momenta"].tolist())
 
     #split enemy_momenta into seperate columns
     enemy_momenta.columns = [f"enemy{i}_momentum" for i in enemy_momenta.columns]
     for c in enemy_momenta.columns:
 
         #split enemy_momentum coordinates into seperate columns
-        df[c+'X'] = enemy_momenta[c].str[0]
-        df[c+'Y'] = enemy_momenta[c].str[1]
+        _df[c+'X'] = enemy_momenta[c].str[0]
+        _df[c+'Y'] = enemy_momenta[c].str[1]
 
     #split enemy screen position into seperate columns
-    enemy_screen_positions = pd.DataFrame(df["enemy_positions_screen"].tolist())
+    enemy_screen_positions = pd.DataFrame(_df["enemy_positions_screen"].tolist())
     enemy_screen_positions.columns = [f"enemy{i}_position_screen" for i in enemy_screen_positions.columns]
 
     #calculate distance between players and enemies
@@ -41,14 +37,14 @@ for line in sys.stdin:
         x = enemy_screen_positions[c].str[0].iloc[0]
         y = enemy_screen_positions[c].str[1].iloc[0]
 
-        df[c+" _distance"] = math.sqrt((x - playerScreenX)**2 + (y - playerScreenY)**2)
+        _df[c+" _distance"] = math.sqrt((x - playerScreenX)**2 + (y - playerScreenY)**2)
 
     #split player momentum into seperate columns
-    df["player_momentumX"] = df["player_momentum"].str[0]
-    df["player_momentumY"] = df["player_momentum"].str[0]
+    _df["player_momentumX"] = _df["player_momentum"].str[0]
+    _df["player_momentumY"] = _df["player_momentum"].str[0]
 
     #drop the old columns from the dataframe
-    df.drop(["enemy_momenta", 
+    _df.drop(["enemy_momenta", 
             "enemy_positions_screen", 
             "enemy_positions_grid", 
             "player_position_grid", 
@@ -57,5 +53,16 @@ for line in sys.stdin:
             "player_momentum"],
             axis=1,  
             inplace=True)
+
+    return _df
+    
+for line in sys.stdin: 
+
+    snapshot = json.loads(line)
+    
+    #write snapshot into dataframe
+    df = pd.json_normalize(snapshot)
+    #clean the dataframe
+    df = cleanData(df)
 
     print(df.shape[1], flush=True)
