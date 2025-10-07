7298e1c745e45e295f79ed2c6e0c39ecef210978
viktor brandmaier <viktor.brandmaier@icloud.com>
2025-10-07 13:33:08 +0200

    added warning-suppression

--- model/ml_worker.py
+++ model/ml_worker.py
@@ -1,7 +1,18 @@
 import pacman_env 
+import warnings
+
+#supress future warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
 
 
 env = pacman_env.PacmanEnv()
 
 #DEBUGGING ONLY:
-env.step('[0]')
+
+done = False
+
+while not done:
+    env.step('[0]')
