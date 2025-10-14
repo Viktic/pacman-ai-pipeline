09b4283befe99d7e1dccb5e915a8c164d893ffa3
= <viktor.brandmaier@icloud.com>
2025-10-14 20:07:16 +0200

    added agent and replay buffer integration

--- model/pacman_env.py
+++ model/pacman_env.py
@@ -2,13 +2,24 @@ import pandas as pd
 import feature_engineer
 import numpy as np
 import json
+import replay_buffer
+import agent
 
 
 class PacmanEnv(): 
 
     #constructor
     def __init__(self):
-        pass
+
+        #creates a replay buffer instance with capacity 10.000
+        self.replay_buffer = replay_buffer.ReplayBuffer(10000)
+
+        #creates an agent instance
+        self.agent = agent.Agent()
+
+        self.previous_state = None
+        self.current_state = None
+        self.previous_action = None
     
     #translate the raw observation into a valid observation-format
     def _translate_obs(self, raw): 
@@ -34,25 +45,45 @@ class PacmanEnv():
 
     def _step(self, line):
         
-        #clean the input line
+        #cleans the input line
         line = line.strip()
         if not line:
             return
 
-        #extract json from raw input
+        #extracts json from raw input
         raw_data = json.loads(line)
 
-        #convert the json into observation format
+        #converts the json into observation format
         obs = self._translate_obs(raw_data)
-
+        reward = raw_data.get("reward")
         done = raw_data.get("done")
         truncated = raw_data.get("truncated")
 
-        #automatically reset the game if truncated or terminated
+        #adds the transition tuple to the replay buffer 
+        if self.previous_state is not None and self.previous_action is not None:
+            self.replay_buffer.add(
+                self.previous_state, 
+                self.previous_action, 
+                reward,
+                obs,
+                done
+            )
+
+        #agent selects an action by querying the ql-network
+        action = self.agent.select_action(obs)
+
+        #updates the states
+        self.previous_state = obs
+        self.previous_action = action
+
+        #automatically resets the game if truncated or terminated
         if done or truncated:
+            self.previous_action = None
+            self.previous_state = None
             print("[-1]", flush=True)
         else:
-            #score bounce back
-            print("[0]", flush=True)
+            #action bounce back
+            print(f"[{action}]", flush=True)
 
+        
         return done, truncated
