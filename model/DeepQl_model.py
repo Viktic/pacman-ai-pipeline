f5662bf2dcac555e68edc5d6da02bc95d95dc925
= <viktor.brandmaier@icloud.com>
2025-11-14 08:30:13 +0100

    added orthogonal weight initialization

--- model/DeepQl_model.py
+++ model/DeepQl_model.py
@@ -15,7 +15,15 @@ class NeuralNetwork(nn.Module):
             nn.Linear(256, action_dim)
         )
 
+        self.apply(self._initWeights)
+
     def forward(self, x):
         logits = self.linear_relu_stack(x)
         return logits 
 
+    #orthogonal weight initialization 
+    def _initWeights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.orthogonal_(module.weight)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
