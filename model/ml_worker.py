f6577852b9de300abaa9914524f13a4ec9ee3aba
= <viktor.brandmaier@icloud.com>
2025-11-23 13:53:35 +0100

    added dynamic log-path

--- model/ml_worker.py
+++ model/ml_worker.py
@@ -1,14 +1,23 @@
 import sys
 import warnings
 import logging
+import os 
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 import pacman_env 
 
-#Debugging
-logging.basicConfig(filename='/Users/viktorbrandmaier/Desktop/Pacman-Pipeline/pacman-ai-pipeline/tests/python_worker_debug.log', level=logging.DEBUG)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+eval_dir = os.path.normpath(os.path.join(script_dir, "../eval/"))
+
+#create eval dir if not exists
+os.makedirs(eval_dir, exist_ok=True)
+
+log_path = os.path.join(eval_dir, "log.txt")
+
+#configure logging
+logging.basicConfig(filename=log_path, level=logging.DEBUG)
 
 env = pacman_env.PacmanEnv()
 
